77e580647446465798a5209529f6896a61131b10
Daniel Shimield <dxshim90@gmail.com>
2019-01-27 16:49:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,3 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(fizz)
-  if fizz % 3 == 0
-    return "Fizz"
-    elsif fizz % 5 == 0
-    return "Buzz"
-  end
-  end
-end
-
